9a441d2036e1b454fc14afdfe9fdfdf161a876bf
NarayanNarayan <narayan1011g@gmail.com>
2023-01-09 20:56:48 +0530

    latest

--- ebpf_progs/main0.py
+++ ebpf_progs/main0.py
@@ -9,12 +9,12 @@ from bcc import BPF
 from ctypes import *
 import sys
 print(sys.argv)
-numcol=2
+numcol=3
 header="""
 #include <asm/ptrace.h> // for struct pt_regs
 #include <linux/types.h> // for mode_t
 BPF_PROG_ARRAY(prog_array, 35);
-BPF_ARRAY(themap, u64, 300);
+BPF_ARRAY(themap, u64, 3000);
 BPF_ARRAY(fgs, u64, 10);
 """
 syscall="execve"
@@ -46,10 +46,6 @@ int kprobe__sys_%s(struct pt_regs *ctx,int dirfd,char __user* pathname, int flag
         if(key == NULL ) return 1;
         int k=%d*(*key);
         themap.update(&k,&t);
-        t = bpf_ktime_get_ns();
-        t = bpf_ktime_get_ns();
-        z=299;
-        themap.update(&z,&t);
         %s
     }
     return 0;
@@ -58,18 +54,21 @@ int kretprobe__sys_%s(struct pt_regs *ctx,int dirfd,char __user* pathname, int f
     u64 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
     if(uid==0){
         //fgs.atomic_increment(0);
+        u64 t = bpf_ktime_get_ns();
         int z = 0;
         u64 *key = fgs.lookup(&z);
         if(key == NULL ) return 1;
+        int k=%d*(*key)+2;
+        themap.update(&k,&t);
+
         u64 zz = *key;
         zz++;
         fgs.update(&z,&zz);
-        //u64 t = bpf_ktime_get_ns();
         %s
     }
     return 0;
 }
-"""%(syscall,numcol,"prog_array.call(ctx, 1);" if begin else "",syscall,"prog_array.call(ctx, 1);" if end else "")
+"""%(syscall,numcol,"prog_array.call(ctx, 1);" if begin else "",syscall,numcol,"prog_array.call(ctx, 1);" if end else "")
 
 funcs=""
 
@@ -106,6 +105,10 @@ for i in range(1,num+1):
 print("Compiled")
 def printvals():
     n=b["fgs"][0].value
+    #with open('%s%s.csv'%(syscall,num), 'w') as sys.stdout:
+    print("start time ,%tail call end time ,syscall end time ,time to run tail calls ,time to run syscall ,total time to run"))
     for i in range(n):
-        print(b["themap"][2*i].value,",",b["themap"][2*i+1].value)
-
+        t1=b["themap"][numcol*i].value
+        t2=b["themap"][numcol*i+1].value
+        t3=b["themap"][numcol*i+2].value
+        print("%d ,%d ,%d ,%d ,%d ,%d"%(t1,t2,t3,t2-t1,t3-t2,t3-t1))
